1f76c1c6f01e25ceb30050a1876b1a8c471a5a15
Yamini Jadhav <yamijadhav77@gmail.com>
2026-08-01 14:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.96%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int fib(int n) {
+        if(n==1){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        int arr[]=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<arr.length;i++) {
+             arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+    public static void main(String args[]){
+        Solution sol=new Solution();
+        int n=5;
+        int ans=sol.fib(n);
+        System.out.println(ans);
+}
+}
